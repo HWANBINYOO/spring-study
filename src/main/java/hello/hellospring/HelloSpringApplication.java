9218723f25fb8e93967315f5b51c4ff452e276ed
HWANBINYOO <dngh0825@naver.com>
2025-02-05 22:34:13 +0900

    Fix : bug

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.hello_spring;
+package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
